d6a2f35a7c0ea397084f798a793407521cba811b
Terence Hughes <Terencehughes450@gmail.com>
2023-07-30 15:48:26 -0600

    finished lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 2
 
+let num2 = 31
+
+const multiply = (2 * 31)// Write your code here
+
+let random = Math.floor(Math.random() * 10) + 1
+
+let num3 = 10
+
+let num4 = 6
+
+let mod = num3 - num4
+
+let max = Math.max(5,10,15,20);
 // Write your code here
